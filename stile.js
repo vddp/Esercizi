ad3fbe6db5031ca93a98fc8baf6e9fe6dae0f1dc
Valerio Di Paolo <valeriodipa@hotmail.com>
2012-03-13 12:05:29 +0100

    appunti stile js

---
+++ stile.js
@@ -0,0 +1,22 @@
+/*
+try{
+  p.getDistance();
+}catch(e){
+}
+*/
+
+
+/* Tecnica per racchiudere tutto in un blocco di funzione
+Gli elementi che si vuole siano visibili si legano al parametro export
+
+(function (export) {
+  var a = 1;
+  var b = 2;
+
+  export.f1() = function (){
+
+  }
+}(window));
+
+window.f1
+*/
